6e8090739f4c040d8db09a7bf8c04e3bef98d768
StevenGandon <steven.gandon@epitech.eu>
2024-03-23 17:50:01 +0100

    [ADD] roulette

--- client/scripts/cases.js
+++ client/scripts/cases.js
@@ -19,6 +19,17 @@ function openCase()
         theCase.classList.remove('the-case-animated');
         theCase.classList.add('the-case');
         theCase.classList.remove('the-case-animation-0');
+
+        let i = 0;
+
+        var interval = setInterval(function () {
+            ++i;
+            if (i >= 803)
+                return clearInterval(interval);
+            document.querySelectorAll('.item-btn').forEach((item) => {
+                item.style.transform = `translateX(${i * (15 - i / 100)}px)`;
+            });
+        }, 10);
     });
 }
 
@@ -29,6 +40,117 @@ function loadCases()
     tradeBtn.classList.remove("active");
 
     app.innerHTML = `
+
+<div class="roulette d-flex position-absolute align-items-center justify-content-center">
+    <div class="position-absolute" style="margin-top: 10%; width: 10px; height: 10px; color: red;">^</div>
+    <div class="roulette-container position-relative d-flex align-items-center justify-content-end">
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+        <div class="position-relative m-2 item-btn"></div>
+    </div>
+</div>
 <div style="width: 98%; height: 98%; background: black; border-radius: 20px;">
     <div class="d-flex position-absolute" style="width: calc(100% - 280px - 1.5em); left: calc(280px + 1.5rem);">
         <div class="alert alert-danger mt-3 position-relative" hidden="true" style="width: 80%; margin-left:auto; margin-right: auto;" id="failBox"></div>
@@ -47,7 +169,7 @@ function loadCases()
         <div class="d-flex position-relative m-2 box-btn"></div>
     </div>
 
-    <div class="d-flex align-items-center justify-content-center" style="width: 100%; height: 100%;">
+    <div class="d-flex align-items-center justify-content-center" style="overflow-y: hidden; width: 100%; height: 100%;">
         <img src="/assets/box0.png" class="the-case" id="theCase" onclick="openCase();"/>
     </div>
 </div>
