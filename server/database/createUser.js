eb1518d37843cccbba004d32c2e159d4bf2cef79
kdeboissy <keenan.deboissy@epitech.eu>
2024-03-23 11:19:53 +0100

    [ADD] UserID in DB

--- server/database/createUser.js
+++ server/database/createUser.js
@@ -3,14 +3,30 @@ const { Database, Table } = require("st.db");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
+async function getUserNextUserID()
+{
+    const database = await new Database("users.json");
+    const userID = await database.get("userCount");
+    if (userID === null || userID === undefined)
+    {
+        await database.set("userCount", 1);
+        return (0);
+    } else {
+        await database.set("userCount", userID + 1);
+        return (userID);
+    }
+}
+
 async function createUser(username, password, email)
 {
+    const userID = await getUserNextUserID();
     const db = await new Database("users.json");
     const users = await new Table("users", db);
     const accountCreationAt = new Date().getTime();
     const accountPassword = await bcrypt.hash(password, process.env.SALT);
 
     await users.set(email.replace('"', '\\"'), {
+        userID: userID,
         username: username,
         accountCreationAt: accountCreationAt,
         password: accountPassword,
