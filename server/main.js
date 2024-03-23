49e7e5bc4fe5ab32f32b4862ea6fe4400951165d
kdeboissy <keenan.deboissy@epitech.eu>
2024-03-23 12:31:10 +0100

    [ADD] Option methods for web browser

--- server/main.js
+++ server/main.js
@@ -29,7 +29,7 @@ async function registerRoutes(app, cache)
 function handleOptionRequests(req, res, next)
 {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept', 'X-HTTP-Method-Override', 'Authorization');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-HTTP-Method-Override, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Credentials', 'true');
     if (req.method === 'OPTIONS')
