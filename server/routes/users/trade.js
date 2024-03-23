fd30b2f983060cc54bd8813348d9e0e895cdfd33
kyllian chandelier <kyllian.chandelier@epitech.eu>
2024-03-23 11:41:49 +0100

    [ADD] parsing trade request

--- server/routes/users/trade.js
+++ server/routes/users/trade.js
@@ -1,6 +1,27 @@
 async function trade(req, res)
 {
-    return res.status(501).send({ message: 'Not implemented' });
+    let username = null;
+    let trade_username = null;
+    let itemID = null;
+
+    console.log(req.params.userID);
+    if (req.method !== 'POST')
+        return res.status(401).send({ message: 'Please use POST method' });
+    if (!req.body)
+        return res.status(400).send({ message: 'Please provide data' });
+    if (!req.body.username)
+        return await res.status(400).send({ message: 'Please provide your username' });
+    if (!req.params.userID)
+        return await res.status(400).send({ message: 'Please provide username to trade with' });
+    if (!req.body.itemID)
+        return await res.status(400).send({ message: 'Please provide the item ID' });
+
+    username = req.body.username;
+    trade_username = req.body.trade_username;
+    itemID = req.body.itemID;
+
+
+    return res.status(200).send({ message: 'Not implemented' });
 }
 
 module.exports = {
