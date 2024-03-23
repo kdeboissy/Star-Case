899ef5389c7baa450184a33b72c2cf4f8daca34f
StevenGandon <steven.gandon@epitech.eu>
2024-03-23 12:11:40 +0100

    [ADD] case on menu

--- client/scripts/cases.js
+++ client/scripts/cases.js
@@ -10,6 +10,27 @@ function loadCases()
     tradeBtn.classList.remove("active");
 
     app.innerHTML = `
-<div style="width: 98%; height: 98%; background: black; border-radius: 20px;"></div>
+<div style="width: 98%; height: 98%; background: black; border-radius: 20px;">
+    <div class="d-flex position-absolute" style="width: calc(100% - 280px - 1.5em); left: calc(280px + 1.5rem);">
+        <div class="alert alert-danger mt-3 position-relative" hidden="true" style="width: 80%; margin-left:auto; margin-right: auto;" id="failBox"></div>
+        <div class="alert alert-success mt-3 position-relative" hidden="true" style="position: absolute; width: 80%; margin-left:auto; margin-right: auto;" id="successBox"></div>
+        <div class="alert alert-secondary mt-3" style="display: none; width: 80%; margin-left:auto; margin-right: auto;" id="loadingBox">
+            <div class="loading-dots"></div>
+        </div>
+    </div>
+
+    <div class="d-flex align-items-center justify-content-center position-absolute" style="width: calc(100% - 280px - 1.5em); left: calc(280px + 1.5rem); height: 100px">
+        <div class="d-flex position-relative align-items-center justify-content-center m-2 box-btn">
+            <img src="/assets/box0.png" style="width: 100%;"/>
+        </div>
+        <div class="d-flex position-relative m-2 box-btn"></div>
+        <div class="d-flex position-relative m-2 box-btn"></div>
+        <div class="d-flex position-relative m-2 box-btn"></div>
+    </div>
+
+    <div class="d-flex align-items-center justify-content-center" style="width: 100%; height: 100%;">
+        <img src="/assets/box0.png" class="the-case"/>
+    </div>
+</div>
 `;
 }
