6671fd4758ba98e39f0d18e20f528bc3dabaa61e
kdeboissy <keenan.deboissy@epitech.eu>
2024-03-24 14:53:47 +0100

    [ADD] openCase linked to 'addInInventory'

--- server/routes/openCase.js
+++ server/routes/openCase.js
@@ -1,4 +1,5 @@
 const { getDatabase } = require("../database/getDatabase");
+const { addItemInInventory } = require("../database/inventory");
 const { getRandomFromStars } = require("../random");
 
 async function getProbability(type)
@@ -14,6 +15,12 @@ async function openCase(req, res, cache)
     let listItems = [];
     let keys = Object.keys(items);
 
+    if (req.method !== 'GET')
+        return res.status(401).send({ message: 'Please use GET method' });
+    const userID = await checkToken(req.headers);
+    if (userID === -1)
+        return res.status(401).send({ message: 'Unauthorized' });
+
     for (let index = 0; index < keys.length; ++index) {
         const item = keys[index];
 
@@ -25,6 +32,7 @@ async function openCase(req, res, cache)
     }
 
     let pickRandomItem = listItems[random % listItems.length];
+    await addItemInInventory(userID, pickRandomItem);
 
     return res.status(200).send({ item: items[pickRandomItem] });
 }
